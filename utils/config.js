f49562f294c8b8794db0994773454a2b1253fb7d
TengYingWei <734970600@qq.com>
2020-05-14 18:54:08 +0800

    improve course page

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@ export default {
     name: '魔力橙',
     logo: 'http://boringcdn.nanningboring.com/Flc0m7az2_GIAh9ai9bHYAgVHOXn',
     apiUrl: 'https://ml.gxsolution.cn/api/mall/', // 正式服务器
-    apiTestUrl: 'https://mp.morangefitness.com/api/mall/', // 测试服务器
+    apiTestUrl: 'http://127.0.0.1:8000/api/mall/', // 测试服务器
     wxPayUrl: 'https://ml.gxsolution.cn/wxMpPay', // 微信支付地址
     fundebugApikey: 'ff717789e60a82896db090dfd919f23fe4634bc69f9688f2df79e944fc26dce5',
     minisessionKey: 'minisession', // 小程序Storage缓存的minisessionKey
